010c1ecd907859808c422a5071bc4e64ed823f35
gtstride <lewisagho47@gmail.com>
2021-06-20 21:24:33 +0100

    Inital commit for start the project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <h2>Learning react has got to be the best thing That has happened to me in decades
+
+        <blockquote>
+          This is where we begin blessing's web app from
+          <aside>Nothing takes God by surprise</aside>
+        </blockquote>
+      </h2>
+      This is an initial commit
+    </div>
+  );
+}
+
+export default App;
