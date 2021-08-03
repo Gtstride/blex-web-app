30e3f8a0dfab1292a66ebf05799ee2f3c7f20ca0
gtstride <lewisagho47@gmail.com>
2021-08-04 00:14:54 +0100

    Fix host error3

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
+import Home from "./pages/home/Home"
 import Header from "./components/Header/Header"
 import AboutUs from "./pages/aboutUs/AboutUs"
 import ContactUs from "./pages/contact-us/ContactUs"
-import Home from "./pages/home/Home"
+
 
 // import PopularCourse from "./views/popular-course/PopularCourse"
 
